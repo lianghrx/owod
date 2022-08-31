9cf2c275afc6ae56abbf8af87558c92e30700202
lianghr <lianghr1988@gmail.com>
2022-08-31 20:13:25 +0800

    modify visualize.py

--- visualise_detections.py
+++ visualise_detections.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import os
 import torch
@@ -27,15 +29,18 @@ def compute_prob(x, distribution):
     end_x = x + eps_radius
     step = (end_x - start_x) / num_eval_points
     dx = torch.linspace(x - eps_radius, x + eps_radius, num_eval_points)
-    pdf = distribution.log_prob(dx).exp()
+    try:
+        pdf = distribution.log_prob(dx).exp()
+    except ValueError:
+        pdf = float('nan')
     prob = torch.sum(pdf * step)
     return prob
 
 
 def update_label_based_on_energy(logits, classes, unk_dist, known_dist):
-    unknown_class_index = 80
+    unknown_class_index = cfg.MODEL.ROI_HEADS.NUM_CLASSES - 1
     cls = classes
-    lse = torch.logsumexp(logits[:, :5], dim=1)
+    lse = torch.logsumexp(logits[:, :cfg.OWOD.CUR_INTRODUCED_CLS], dim=1)
     for i, energy in enumerate(lse):
         p_unk = compute_prob(energy, unk_dist)
         p_known = compute_prob(energy, known_dist)
@@ -46,57 +51,48 @@ def update_label_based_on_energy(logits, classes, unk_dist, known_dist):
             cls[i] = unknown_class_index
     return cls
 
-# Get image
-fnum = '348006'
-file_name = '000000' + fnum
-im = cv2.imread("/home/fk1/workspace/OWOD/datasets/VOC2007/JPEGImages/" + file_name + ".jpg")
-# model = '/home/fk1/workspace/OWOD/output/old/t1_20_class/model_0009999.pth'
-# model = '/home/fk1/workspace/OWOD/output/t1_THRESHOLD_AUTOLABEL_UNK/model_final.pth'
-# model = '/home/fk1/workspace/OWOD/output/t1_clustering_with_save/model_final.pth'
-# model = '/home/fk1/workspace/OWOD/output/t2_ft/model_final.pth'
-# model = '/home/fk1/workspace/OWOD/output/t3_ft/model_final.pth'
-model = '/home/fk1/workspace/OWOD/output/t4_ft/model_final.pth'
-cfg_file = '/home/fk1/workspace/OWOD/configs/OWOD/t1/t1_test.yaml'
 
+# Get image
+datasets_root = os.getenv('DETECTRON2_DATASETS', 'datasets')
+file_name = "18-57-24"
+im = cv2.imread(os.path.join(datasets_root, 'VOC2007/JPEGImages', file_name + '.jpg'))
+
+# model = './output/t2_ft/model_final.pth'
+# model = './output/t3_ft/model_final.pth'
+model = './output/t1/model_final.pth'
+cfg_file = './configs/OWOD_TOOLKIT/t1/t1_test.yaml'
 
 # Get the configuration ready
 cfg = get_cfg()
 cfg.merge_from_file(cfg_file)
 cfg.MODEL.WEIGHTS = model
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.61
-# cfg.MODEL.ROI_HEADS.POSITIVE_FRACTION = 0.8
-cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.4
-
-# POSITIVE_FRACTION: 0.25
-# NMS_THRESH_TEST: 0.5
-# SCORE_THRESH_TEST: 0.05
-# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 21
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.4
+cfg.MODEL.ROI_HEADS.POSITIVE_FRACTION = 0.8
+cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.5
 
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
 
-print('Before' + str(outputs["instances"].pred_classes))
+print('Before' + str(outputs['instances'].pred_classes))
 
-param_save_location = os.path.join('/home/fk1/workspace/OWOD/output/t1_clustering_val/energy_dist_' + str(20) + '.pkl')
-params = torch.load(param_save_location)
+params = torch.load(os.path.join('./output/t1_final/energy_dist_' + str(21) + '.pkl'))
 unknown = params[0]
 known = params[1]
 unk_dist = create_distribution(unknown['scale_unk'], unknown['shape_unk'], unknown['shift_unk'])
 known_dist = create_distribution(known['scale_known'], known['shape_known'], known['shift_known'])
 
-instances = outputs["instances"].to(torch.device("cpu"))
-dev =instances.pred_classes.get_device()
+instances = outputs['instances'].to(torch.device('cpu'))
+dev = instances.pred_classes.get_device()
 classes = instances.pred_classes.tolist()
-logits = instances.logits
-classes = update_label_based_on_energy(logits, classes, unk_dist, known_dist)
-classes = torch.IntTensor(classes).to(torch.device("cuda"))
-outputs["instances"].pred_classes = classes
+classes = update_label_based_on_energy(instances.logits, classes, unk_dist, known_dist)
+classes = torch.IntTensor(classes).to(torch.device('cuda'))
+outputs['instances'].pred_classes = classes
 print(classes)
-print('After' + str(outputs["instances"].pred_classes))
+print('After' + str(outputs['instances'].pred_classes))
 
 
-v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=2)
 v = v.draw_instance_predictions(outputs['instances'].to('cpu'))
 img = v.get_image()[:, :, ::-1]
-cv2.imwrite('output_' + file_name + '.jpg', img)
+cv2.imwrite(os.path.join('./output/' + file_name + '.jpg'), img)
 
