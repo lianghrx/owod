bcc0e985eaea551d4b99310c203503530bb4f0d7
lianghr <lianghr1988@gmail.com>
2022-08-31 16:01:41 +0800

    fix files copy bash

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD_
 
 
 ## Task 2
-#cp -r ./output/t1 ./output/t2
+#mkdir -p ./output/t2 && cp -r ./output/t1/* ./output/t2/
 #
 #python tools/train_net.py --num-gpus 8 --resume --config-file ./configs/OWOD_TOOLKIT/t2/t2_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2" MODEL.WEIGHTS "./output/t2/model_final.pth"
 #
-#cp -r ./output/t2 ./output/t2_ft
+#mkdir -p ./output/t2_ft && cp -r ./output/t2/* ./output/t2_ft/
 #
 #python tools/train_net.py --num-gpus 8 --resume --config-file ./configs/OWOD_TOOLKIT/t2/t2_ft.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_ft" MODEL.WEIGHTS "./output/t2_ft/model_final.pth"
 #
@@ -23,11 +23,11 @@ python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD_
 #
 #
 ## Task 3
-#cp -r ./output/t2_ft ./output/t3
+#mkdir -p ./output/t3 && cp -r ./output/t2_ft/* ./output/t3/
 #
 #python tools/train_net.py --num-gpus 8 --resume --config-file ./configs/OWOD_TOOLKIT/t3/t3_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3" MODEL.WEIGHTS "./output/t3/model_final.pth"
 #
-#cp -r ./output/t3 ./output/t3_ft
+#mkdir -p ./output/t3_ft && cp -r ./output/t3/* ./output/t3_ft/
 #
 #python tools/train_net.py --num-gpus 8 --resume --config-file ./configs/OWOD_TOOLKIT/t3/t3_ft.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_ft" MODEL.WEIGHTS "./output/t3_ft/model_final.pth"
 #
